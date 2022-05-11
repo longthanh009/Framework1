11f1632c72c9b4e95668653e26630fb758626021
khuatlong0902 <khuatlong0902@gmail.com>
2022-05-11 21:01:39 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HelloComponent } from './components/hello/hello.component';
+import { HearderComponent } from './components/hearder/hearder.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    HearderComponent,
+    FooterComponent,
+    DashboardComponent,    
   ],
   imports: [
     BrowserModule,
